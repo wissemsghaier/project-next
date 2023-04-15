06d9b73ce3881f3b9f6b88c223e8e337725c37df
Wissem Sghaïer <wissemsghaier2000@gmail.com>
2023-04-15 23:47:02 +0100

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+}
+
